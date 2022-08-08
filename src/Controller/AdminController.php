0078b9f562e82745709fda8726a1ecf5b20fb1bf
dldvlpr <dormoy.dev@gmail.com>
2022-08-08 10:35:03 +0200

    some fixes

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,11 +2,9 @@
 
 namespace App\Controller;
 
-use App\Form\SearchOrderFormType;
 use App\Repository\CakeRepository;
 use App\Repository\OrderRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -29,12 +27,11 @@ class AdminController extends AbstractController
     }
 
     #[Route('/commandes', name: 'orders')]
-    public function ordersIndex(OrderRepository $orderRepository, Request $request): Response
+    public function ordersIndex(OrderRepository $orderRepository): Response
     {
+
         $orders = $orderRepository->findAll();
 
-        $searchForm = $this->createForm(SearchOrderFormType::class);
-        $searchForm->handleRequest($request);
         return $this->render('admin/orderslist.html.twig', [
             'orders' => $orders,
         ]);
