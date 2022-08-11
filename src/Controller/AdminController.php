8acfce4ddbc7f328c65af419daa226eae81cdbba
dldvlpr <dormoy.dev@gmail.com>
2022-08-11 10:39:47 +0200

    search system on cake for admin panel

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -3,9 +3,12 @@
 namespace App\Controller;
 
 use App\Form\OrderFormType;
+use App\Form\SearchCakeFormType;
 use App\Form\SearchUserFormType;
 use App\Repository\CakeRepository;
+use App\Repository\DepartmentRepository;
 use App\Repository\UserRepository;
+use App\Service\CakeSearchService;
 use App\Service\OrderSearchService;
 use App\Service\UserSearchService;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
@@ -24,11 +27,42 @@ class AdminController extends AbstractController
     }
 
     #[Route('/gateaux', name: 'cakes', methods: ['POST', 'GET'])]
-    public function cakesIndex(CakeRepository $cakeRepository): Response
-    {
-        $cakes = $cakeRepository->findAll();
-        return $this->render('admin/cakeslist.html.twig', [
+    public function cakeIndex(
+        Request $request,
+        CakeSearchService $cakeSearchService,
+        DepartmentRepository $departmentRepository
+    ): Response {
+        // fetching all departments for the scrolling menu
+        $departmentsDisplay = $departmentRepository->findAll();
+        // creating form
+        $searchForm = $this->createForm(SearchCakeFormType::class);
+        $searchForm->handleRequest($request);
+        // initializing search and department variables before the form is set
+        $search = "";
+        $department = "";
+
+        if ($searchForm->isSubmitted() && $searchForm->isValid()) {
+            $searchRequest = $request->get('search_cake_form');
+
+            // some bricolage to please phpcs
+            if (is_array($searchRequest)) {
+                $search = $searchRequest['search'];
+
+                // for homepage buttons (which don't take departments into account)
+                if (isset($searchRequest['department'])) {
+                    $department = $searchRequest['department'];
+                }
+            }
+        }
+        // calling the CakeSearchService
+
+        $cakes = $cakeSearchService->cakeSearch($search, $department);
+
+        return $this->renderForm('admin/cakeslist.html.twig', [
             'cakes' => $cakes,
+            'searchForm' => $searchForm,
+            'search' => $search,
+            'departments' => $departmentsDisplay,
         ]);
     }
 
--- src/Form/SearchCakeFormType.php
+++ src/Form/SearchCakeFormType.php
@@ -28,7 +28,6 @@ class SearchCakeFormType extends AbstractType
                     'choice_label' => 'displayName'
                 ]
             )
-            ->setAction('/gateau/')
             ->setMethod('POST');
     }
 
